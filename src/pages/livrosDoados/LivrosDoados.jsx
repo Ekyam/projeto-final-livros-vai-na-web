d99405375d05d3989753912951ee74afc4f614bf
Ekyam <maykao125@gmail.com>
2025-04-04 23:01:17 -0300

    Atualiza funcionalidades de exibir e cadastrar livros

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,19 +1,34 @@
 import s from './LivrosDoados.module.scss';
-import livroProtagonista from '../../assets/livroProtagonista.png';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-export default function LivrosDoados(){
-    return(
-    <section className={ s.livrosDoadosSection}>
+export default function LivrosDoados() {
+
+    const [livros, setLivros] = useState([])
+
+    const getLivros = async () => {
+        const resposta = await axios.get("https://desafio-livros-api.onrender.com/livros")
+        setLivros(resposta.data)
+    }
+
+    useEffect(() => {
+        getLivros()
+        alert("useEffect executou")
+    }, [])
+
+    return (
+        <section className={s.livrosDoadosSection}>
             <h2>Livros Doados</h2>
             <section className={s.containerCards}>
-                <section>
-                <img src={ livroProtagonista} alt="Imagem ilustrativa de um livro que foi doado" />
-                <div>
-                <h3>O protagonista</h3>
-                <p>Susanne Andrade</p>
-                <p>Ficção</p>
-                </div>
-                </section>
+                {livros.map((item) => (
+                    <section>
+                        <div>
+                            <h3>{item.titulo}</h3>
+                            <img src={item.image_url} alt={item.titulo} />
+                        </div>
+                    </section>
+                ))}
+
             </section>
         </section>
     )
